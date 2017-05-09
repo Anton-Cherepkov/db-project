83b2f9468fba687020e27e3d1376c37b321f532c
acherepko <anthonycherepkov@gmail.com>
2017-05-09 14:46:53 +0300

    entry_* usage example

--- client/client.py
+++ client/client.py
@@ -1,7 +1,19 @@
 from session import Session
+from entry_teacher import Teacher
+from entry_class import Class
+from entry_subject import Subject
+from entry_student import Student
+from entry_schedule import Schedule
+from entry_mark import Mark
 
 def main():
     session = Session()
+
+    subject = Subject(session, 107)
+    print(subject.get(Subject.name))
+    subject.set(Subject.name, 'Русский язык')
+    session.connection.commit()
+    print(subject.get(Subject.name))
 
 if __name__ == "__main__":
     main()
